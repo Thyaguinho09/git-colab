30fc43e245346ec43ee37c23e99c2ef52db9a48d
Klayvertone <klayvertone@gmail.com>
2026-03-20 14:26:00 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Mostra mensagem
+console.log("Olá, Mundo!");
+
+// Pede dois números ao usuário
+let num1 = Number(prompt("Digite o primeiro número:"));
+let num2 = Number(prompt("Digite o segundo número:"));
+
+// Calcula a soma
+let soma = num1 + num2;
+
+// Mostra o resultado
+alert("A soma é: " + soma);
